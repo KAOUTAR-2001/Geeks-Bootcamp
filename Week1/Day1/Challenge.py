5ac30469e7057aabaf645fa2c577508751f21111
KAOUTAR-2001 <elassalkaoutar@gmail.com>
2025-06-19 15:38:19 +0100

    ChLNG

---
+++ Week1/Day1/Challenge.py
@@ -0,0 +1,18 @@
+#1
+
+print("challenge1")
+number = int(input("Enter a number: "))
+length = int(input("Enter the length of the list: "))
+multiples_list = [number * i for i in range(1, length + 1)]
+
+print(multiples_list)
+
+#2
+
+print("challenge2")
+print("")
+word = input("Enter a word: ")
+
+new_word = "".join([word[i] for i in range(len(word)) if i == 0 or word[i] != word[i - 1]])
+
+print(new_word)
